95d286fe5b94a3a8ddbc8a4584d7f98aaf90f2a0
shamelessApathy <bcm811@gmail.com>
2018-06-29 11:04:54 -0600

    breaking on setState to rebuild the ToDos andCompletes objects. Some inherent removeChildNode function for react is breaking it, need to find a nother way to remove it

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class Tasks extends React.Component {
 	render(props)
 	{
 		return (
-			this.props.value
+				this.props.value
 			);
 	}
 }
@@ -48,7 +48,7 @@ class DropIt extends React.Component {
 			<Tasks
 				value={tasks_array}
 			/>
-			)
+			);
 	}
 	renderCompleteTasks()
 	{
@@ -63,22 +63,72 @@ class DropIt extends React.Component {
 			<Tasks
 				value={tasks_array}
 			/>
-			)
+			);
 	}
 	rebuildCompletes()
 	{
 		let container = document.getElementById('complete-div');
-		let children = container.getElementsByClassName('task');
-		console.log(children.length);
+		let children = container.children;
+		console.log(children);
+		let completes_array = [];
 		for (let i = 0; i < children.length; i++)
 		{
-			console.log(children[i]);
-		}
+			if (children[i].className === "outer-tasks")
+			{
+				let secondChildren = children[i].children;
+				for (let j = 0; j < secondChildren.length; j++)
+				{
+					let dataText = secondChildren[j].getAttribute('data-text');
+					let id = secondChildren[j].id;
 
+					completes_array.push({"text":dataText,"class":"task","id":id});
+				}
+			}
+			else
+			{
+				let dataText = children[i].getAttribute('data-text');
+				let id = children[i].id;
+				completes_array.push({"text":dataText, "class":"task","id":id});
+			}
+		}
+		return completes_array;
+
+	}
+	rebuildToDos()
+	{
+		let container = document.getElementById('todo-div');
+		let children = container.children;
+		let todos_array = [];
+		for (let i = 0; i < children.length; i++)
+		{
+			if (children[i].className === "outer-tasks")
+			{
+				let secondChildren = children[i].children;
+				for (let j = 0; j < secondChildren.length; j++)
+				{
+					let dataText = secondChildren[j].getAttribute('data-text');
+					let id = secondChildren[j].id;
+
+					todos_array.push({"text":dataText,"class":"task","id":id});
+				}
+			}
+			else
+			{
+				let dataText = children[i].getAttribute('data-text');
+				let id = children[i].id;
+				todos_array.push({"text":dataText, "class":"task","id":id});
+			}
+		}
+		return todos_array;
 	}
 	rebuildTasks()
 	{
-		this.rebuildCompletes();
+		let completes = this.rebuildCompletes();
+		let todos = this.rebuildToDos();
+		this.setState({
+			todos: todos,
+			completes: completes 
+		})
 	}
 	allowDrop(ev)
 	{
@@ -91,28 +141,43 @@ class DropIt extends React.Component {
 	}
 	drop(ev)
 	{
-				console.log(ev.target.className);
-		if (ev.target.className === "completed-container")
-		{
-			this.rebuildTasks();
-		}
+
 		ev.preventDefault();
 		let data = ev.dataTransfer.getData("text");
 		console.log(data);
 		ev.target.appendChild(document.getElementById(data));
+
+		// Needs to be below appendChild to count right number of child divs
+		console.log(ev.target.className);
+		if (ev.target.className === "completed-container")
+		{
+			this.rebuildTasks();
+		}
 	}
+	componentDidCatch(error, info) 
+	{
+    	// Display fallback UI
+    	this.setState({
+			todos: [],
+			completes: []
+    	 });
+    	console.log(error);
+
+  	}
 	render()
 	{
 		return(
 			<div>
 				<div className="dropit-container">
 					<h4 className="title">DropIt Component</h4>
+					<h4 className="title-left">To Do's</h4>
 					<div id="todo-div" className="todo-container" onDragOver={(event)=> this.allowDrop(event)} onDrop={(event) => this.drop(event)}>
-						<h4 className="title">To Do's</h4>
+						
 						{this.renderToDoTasks()}
 					</div>
+					<h4 className="title-right">Completed</h4>
 					<div id="complete-div" className="completed-container" onDragOver={(event)=> this.allowDrop(event)} onDrop={(event) => this.drop(event)}>
-						<h4 className="title">Completed</h4>
+						
 						{this.renderCompleteTasks()}
 					</div>
 				</div>
